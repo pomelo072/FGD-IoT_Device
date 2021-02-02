2001c9939b2b81c80303f98d3389405854b74f6f
pomelo072 <2641316287@qq.com>
2021-02-02 18:32:58 +0800

    add ConnectWIFI.py

---
+++ FGD-IoT/ConnectWIFI.py
@@ -0,0 +1,13 @@
+import network
+
+
+def do_connect(essid, password):
+    sta_if = network.WLAN(network.STA_IF)
+    if not sta_if.isconnected():
+        print('connecting to network...')
+        sta_if.active(True)
+        sta_if.connect(essid, password)
+        while not sta_if.isconnected():
+            pass
+    print("WIFI connected.")
+    print('network config:', sta_if.ifconfig())
