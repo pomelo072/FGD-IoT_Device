a181c6dd9ca3860ddf6b1cc26a4e8f2a834da4c6
pomelo072 <2641316287@qq.com>
2021-02-02 18:27:32 +0800

    add close file. Stupid...

--- FGD-IoT/LoadJSON.py
+++ FGD-IoT/LoadJSON.py
@@ -2,10 +2,13 @@ import ujson
 
 
 def LoadJSON():
-    config = open("config.json", "r", encoding="utf-8")
-    loaded_config = ujson.load(config)
-    WIFISSID = loaded_config['WIFISSID']
-    WIFIPassWord = loaded_config['WIFIPassWord']
-    ReloadTime = loaded_config['ReloadTime']
+    try:
+        config = open("config.json", "r", encoding="utf-8")
+        loaded_config = ujson.load(config)
+        WIFISSID = loaded_config['WIFISSID']
+        WIFIPassWord = loaded_config['WIFIPassWord']
+        ReloadTime = loaded_config['ReloadTime']
+    finally:
+        config.close()
     print("WIFI Config: {0} @ {1}    ReloadTime: {2}".format(WIFISSID, WIFIPassWord, ReloadTime))
     return WIFISSID, WIFIPassWord, ReloadTime
