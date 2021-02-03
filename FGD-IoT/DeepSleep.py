4432311e8b3f76686e7fc38e8f19ca3757633d3f
pomelo072 <2641316287@qq.com>
2021-02-03 11:57:25 +0800

    add DeepSleep, use lower power

---
+++ FGD-IoT/DeepSleep.py
@@ -0,0 +1,13 @@
+import machine
+
+
+def DeepSleep(time):
+    rtc = machine.RTC()
+    rtc.irq(trigger=rtc.ALARM0, wake=machine.DEEPSLEEP)
+
+    if machine.reset_cause() == machine.DEEPSLEEP_RESET:
+        print('woke from a deep sleep')
+
+    rtc.alarm(rtc.ALARM0, time * 1000)
+
+    machine.deepsleep()
