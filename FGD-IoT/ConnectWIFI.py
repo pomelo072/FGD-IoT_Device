b3ed4aeed53b29db2b2f37c09a3cafa5be367605
pomelo072 <2641316287@qq.com>
2021-02-04 21:50:29 +0800

    remove print to stdout, show network config to lcd.

--- FGD-IoT/ConnectWIFI.py
+++ FGD-IoT/ConnectWIFI.py
@@ -1,19 +1,20 @@
 import network
 import machine
 import ntptime
+import SensorController
 
 
 def do_connect(essid, password):
     sta_if = network.WLAN(network.STA_IF)
     if not sta_if.isconnected():
-        print('connecting to network...')
+        SensorController.Display_LCD1602('connecting to\nnetwork...', 2)
         sta_if.active(True)
         sta_if.connect(essid, password)
         while not sta_if.isconnected():
             pass
-    print("WIFI connected.")
-    print('network config:', sta_if.ifconfig())
+    SensorController.Display_LCD1602("WIFI connected.", 1)
+    SensorController.Display_LCD1602('network config\n' + sta_if.ifconfig()[0], 2)
 
     ntptime.settime()
-    rtc = machine.RTC()
-    print(rtc.datetime())
+
+
